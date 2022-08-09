757ff837702d9b862f284d2d782a6b371815d501
Miyushan <smiushan2015@gmail.com>
2022-08-09 10:16:56 +0530

    axios removed and cors

--- BACK_END/app.js
+++ BACK_END/app.js
@@ -12,12 +12,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
-app.use(cors({ origin: true, credentials: true }));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
